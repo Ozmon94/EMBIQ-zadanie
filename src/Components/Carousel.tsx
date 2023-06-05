43046f148264040a7dedf419b13b3a7f3c788d38
Szymon Moskwa <szymonmoskwa@gmail.com>
2023-06-05 12:59:02 +0200

    add: draggable to carousel

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -2,24 +2,47 @@ import { Box, Button, Typography } from "@mui/material";
 
 import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { StoreContext } from "../store/StoreProvider";
 import { StoreType } from "../types/storeType";
+import Draggable, { DraggableEventHandler } from "react-draggable";
 
 export default function Carousel() {
   const { circuit, removeHandler, addHandler } = useContext(
     StoreContext
   ) as StoreType;
 
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleOnStop: DraggableEventHandler = (e, data) => {
+    if (data.x <= -10) {
+      addHandler();
+    }
+    if (data.x >= 10) {
+      removeHandler();
+    }
+    setPosition({ x: 0, y: 0 });
+  };
+
   if (!circuit) return <Box></Box>;
   return (
     <Box sx={{ width: "100%" }}>
-      <Box sx={{ border: "1px solid", borderColor: "primary.light" }}>
-        <img
-          src={circuit.image}
-          alt="Melbourne Grand Prix Circuit"
-          style={{ width: "100%" }}
-        />
+      <Box
+        sx={{
+          border: "1px solid",
+          borderColor: "primary.light",
+          overflow: "hidden",
+        }}
+      >
+        <Draggable axis="x" onStop={handleOnStop} position={position}>
+          <Box>
+            <img
+              src={circuit.image}
+              alt="Melbourne Grand Prix Circuit"
+              style={{ width: "100%", pointerEvents: "none" }}
+            />
+          </Box>
+        </Draggable>
       </Box>
 
       <Box
